f4d1ac8b4f10c16e433675f5a4ea8ce88b4e0602
Mihail Nica <110503052+mihailnicamn@users.noreply.github.com>
2022-12-31 22:10:41 +0200

    _

--- manager.js
+++ manager.js
@@ -24,14 +24,30 @@ const testRun = (script_data,handler) => {
 }
 const installDependencies = (scripts,handler) => {
     const { spawn } = require('child_process');
-    const child = spawn('npm', ['install', '--save', ...scripts]);
+    const cmd = `npm install --save ${scripts.join(" ")}`
+    const child = spawn(cmd, [], { shell: true, stdio: 'pipe' });
     child.stdout.on('data', (data) => {
-        handler(data);
+        handler(data.toString());
     });
     child.stderr.on('data', (data) => {
-        handler(data);
+        handler(data.toString());
     });
     child.on('close', (code) => {
-        handler(code);
+        handler(code.toString());
     });
 }
+const getDependeces = (script_data) => {
+    //get dependence name from require() function
+    const regex = /require\((.*?)\)/g;
+    const matches = script_data.match(regex);
+    const dependencies = [];
+    if (matches) {
+        matches.forEach((item) => {
+            dependencies.push(item.replace("require(", "").replace(")", "").replace(/'/g, "").replace(/"/g, ""));
+        });
+    }
+    return dependencies;
+}
+installDependencies(getDependeces(`require('body-parser');require('express');require('js-beautify');require('js-yaml');require('crypto-js');require('puppeteer');require('node-cron');require('express-basic-auth)`), (data) => {
+    console.log(data)
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+#Browser Version 
+- available at: [https://mihailnicamn.github.io/crawler_manager/](https://mihailnicamn.github.io/crawler_manager/)
+- it will allow you build puppeteer scripts, you'll need to download it and run it locally
+
+#Full Environment
+- clone this repo
+- run `npm install`
+- set your username and password in .env file
+- run `npm start`
+- open [http://<<ip>>:<<port>>](http://<<ip>>:<<port>>) | (default) [http://localhost:3000](http://localhost:3000)
+- it will allow you build puppeteer scripts with test runs, schedule them and run them on a server
